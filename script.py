a4e3d75cfdb000c603c2eb53f4a1755c1def1a15
elrapace <nicoangeli2002@gmail.com>
2025-07-14 12:36:17 +0200

    Update

--- script.py
+++ script.py
@@ -2,23 +2,23 @@ import sys, os
 
 #CONTROLLO IL SECONDO AROGMENTO DOPO IL NOME DELLO SCRIPT CHE PRENDO IL PERCORSO IN CUI ESEGUIRE LO SCRIPT
 if len(sys.argv) < 2:
-	print('ERROR, NO ARGUMENTS!')
+	print('--> ERROR, NO ARGUMENTS!')
 	sys.exit(1) #USCITA CON CODICE DI ERRORE 1
 	
-print('SCRIPT RUN')
+print('--> SCRIPT RUN')
 
 #CAMBIO DIRECTORY DI LAVORO
 os.chdir(sys.argv[1])
-print(f'CURRENT DIRECTORY: {os.getcwd()}')
+print(f'--> CURRENT DIRECTORY: {os.getcwd()}')
 
 #ESEGUO COMANDI GIT
 os.system('git add --all')
-print('ADD, OK!')
+print('--> ADD, OK!')
 
 #ESEGUO IL COMMIT
 os.system('git commit -m "Update"')
-print('COMMIT, OK!')
+print('--> COMMIT, OK!')
 
 #GIT PUSH
 os.system('git push')
-print('PUSH, OK!')
+print('--> PUSH, OK!')
